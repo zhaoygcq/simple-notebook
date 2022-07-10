c41fb7c608518bd5f7b6a3b123985b83e03d6ff3
zhaoygcq <1060401583@qq.com>
2022-07-10 10:48:46 +0800

    chore: 尝试纯前端实现页面转pdf

--- src/views/index.js
+++ src/views/index.js
@@ -1,3 +1,6 @@
+import { dialog } from "@tauri-apps/api";
+import html2canvas from "html2canvas";
+import jsPDF from "jspdf";
 export default function pdfPlugin() {
     // 后续需要解决的问题： 如何转md到pdf
     return {
@@ -8,9 +11,13 @@ export default function pdfPlugin() {
                 cheatsheet: "$".concat('test', "$"),
                 handler: {
                     type: 'action',
-                    click: ({editor}) => {
-                        let content = editor.getValue();
-                        console.log("test plugin======", content);
+                    click: ({editor, root}) => {
+                        let dom = root.querySelector(".bytemd-preview");
+                        html2canvas(dom)
+                          .then(canvas => {
+                            let url = canvas.toDataURL();
+                          })
+                        console.log("test plugin======", );
                         editor.focus();
                     }
                 }
